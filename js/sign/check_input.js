dff58a012292ec4d5f721d4435daf38efbdc24f7
Jian Jang <pureunnabi@gmail.com>
2024-01-17 15:46:21 +0900

    서버와 로그인 연동

--- js/sign/check_input.js
+++ js/sign/check_input.js
@@ -11,3 +11,37 @@ function checkInputs() {
         signinBtn.classList.add('disabled');
     }
 }
+
+function submitLogin() {
+    var id = document.getElementById("id").value;
+    var pw = document.getElementById("pw").value;
+  
+      // 클라이언트 로그인 후
+    axios
+    .post("http://localhost:3000/user/login", {
+      userid: id,
+      pw: pw,
+    })
+    .then((response) => {
+      const token = response.data.token;
+
+      localStorage.setItem("token", token);
+      localStorage.setItem("userid", id);
+  
+      alert("로그인 되었습니다");
+  
+    })
+    .catch((error) => {
+      if (error.response && error.response.status === 401) {      
+        alert("아이디 또는 비밀번호가 일치하지 않습니다.");
+  
+      } else {
+        alert("로그인 에러가 발생했습니다.");
+  
+        console.error("Error during logi?n:", error.message);
+      }
+    });
+  
+  
+  }
+  
