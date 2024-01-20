4eafb857b8fe83a2248cbd00214e0b722b58deb0
sg-hwang-333 <hsg151515@naver.com>
2024-01-20 22:06:43 +0900

    [FEAT] 모달 창 구현 완료, 회원가입 및 로그인 ui 완성 (img 제외)

--- js/sign/sign_check_btn.js
+++ js/sign/sign_check_btn.js
@@ -1,3 +1,4 @@
+// 확인 버튼 활성화
 function chkBtnAble() {
     let chk = document.getElementsByClassName('genre-img');
     let chkBtn = document.getElementsByClassName('next-btn')[0];
@@ -13,3 +14,43 @@ function chkBtnAble() {
         }
     }
 }
+
+// 모달 창 띄우기
+function modal() {
+    var zIndex = 9999;
+    var modal = document.getElementsByClassName('modal-container')[0];
+
+    var bg = document.createElement('div');
+    bg.setStyle({
+        position: 'fixed',
+        zIndex: zIndex,
+        left: '0px',
+        top: '0px',
+        width: '100%',
+        height: '100%',
+        overflow: 'auto',
+        backgroundColor: 'rgba(0,0,0,0.2)'
+    });
+    document.body.append(bg);
+
+    modal.querySelector('.goto-signIn').addEventListener('click', function() {
+        bg.remove();
+        modal.style.display = 'none';
+    });
+
+    modal.setStyle({
+        position: 'fixed',
+        display: 'block',
+        zIndex: zIndex + 1,
+
+        top: '50%',
+        left: 'calc(50%)',
+        transform: 'translate(-50%, -50%)',
+        msTransform: 'translate(-50%, -50%)',
+        webkitTransform: 'translate(-50%, -50%)'
+    });
+}
+Element.prototype.setStyle = function(styles) {
+    for (var k in styles) this.style[k] = styles[k];
+    return this;
+};
