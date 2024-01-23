52284a4745dd19b37be60f366bbfa9d11353dbfd
Jian Jang <pureunnabi@gmail.com>
2024-01-23 10:08:58 +0900

    이미지 업로드

--- js/sign/profile_img_change.js
+++ js/sign/profile_img_change.js
@@ -13,3 +13,31 @@ function chkToNext() {
     nextBtn.removeAttribute("disabled");
     nextBtn.classList.remove('disabled');
 }
+
+function uploadImage() {
+    const formData = new FormData(document.getElementById('uploadForm'));
+    const email = localStorage.getItem('email');
+
+    axios.post('http://localhost:3000/user/signup5', formData, {
+        email : email,
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    })
+    .then(response => {
+        console.log('Image upload response:', response.data);
+
+        if (response.data.success) {
+            const imageUrl = response.data.image_url;
+            console.log('Image URL:', imageUrl);
+            document.getElementById('result').innerHTML = 'Image uploaded successfully. Image URL: ' + imageUrl;
+            // location.href='./input_birth.html';
+        } else {
+            console.error('Image upload failed:', response.data.message);
+            document.getElementById('result').innerHTML = 'Image upload failed: ' + response.data.message;
+        }
+    })
+    .catch(error => {
+        console.error('Error during image upload:', error);
+    });
+}
