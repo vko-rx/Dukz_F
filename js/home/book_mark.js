78cc3d0b7031d38869de1e6d00acd5d7d8231b22
sg-hwang-333 <hsg151515@naver.com>
2024-01-24 17:52:40 +0900

    [STYLE] bookmark, heart 이미지 통일

--- js/home/book_mark.js
+++ js/home/book_mark.js
@@ -1,9 +1,9 @@
 function bookmarkChk(obj) {
-    if (obj.src.includes('un_check')) {
-        obj.src = "../../Image/icon/card_icon/bookmark/check.png";
+    if (obj.src.includes('un_fill')) {
+        obj.src = "../../Image/icon/bookmark/fill.png";
         // 카드뉴스를 북마크에 추가
     } else {
-        obj.src = "../../Image/icon/card_icon/bookmark/un_check.png";
+        obj.src = "../../Image/icon/bookmark/un_fill.png";
         // 카드뉴스를 북마크에서 해제
     }
 }
