78edb2fe4e6e3fae48a3427429a65f4cc38f929c
sg-hwang-333 <hsg151515@naver.com>
2024-01-24 12:59:57 +0900

    [FEAT] 여행일정 화면 ui 완성 (기능 X)

---
+++ js/travel_plan/swipe.js
@@ -0,0 +1,8 @@
+new Swiper('.plan-swiper', {
+    spaceBetween: -120, // 슬라이드 사이 여백
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+});
