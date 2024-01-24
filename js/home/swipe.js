687b235dbfb2b00f8c5f0aa81ec42e181d09e3b1
sg-hwang-333 <hsg151515@naver.com>
2024-01-24 09:16:32 +0900

    [FEAT] swipe 수정

--- js/home/swipe.js
+++ js/home/swipe.js
@@ -10,7 +10,7 @@ new Swiper('.swiper', {
 
 new Swiper('.swiper2', {
   // slidesPerView: 2, // 한번에 보여줄 슬라이드 개수
-  spaceBetween: -160, // 슬라이드 사이 여백
+  spaceBetween: -170, // 슬라이드 사이 여백
   centeredSlides: false, // 1번 슬라이드가 가운데 보이기
   freeMode: true,
 });
