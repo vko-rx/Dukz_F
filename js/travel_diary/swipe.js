b068c0b8990632245f6de2abdce21bc650ecce8d
sg-hwang-333 <hsg151515@naver.com>
2024-01-25 13:53:58 +0900

    [FEAT] diary 부분 1차 수정, select_genre 모달 창 오류 수정

---
+++ js/travel_diary/swipe.js
@@ -0,0 +1,8 @@
+new Swiper('.diary-swiper', {
+    spaceBetween: 10, // 슬라이드 사이 여백
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+});
