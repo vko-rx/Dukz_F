96f095897a09dcf53cbb0c1c5bd15858c26c9d21
sg-hwang-333 <hsg151515@naver.com>
2024-01-26 00:33:32 +0900

    [FEAT] 여행일지 글 부분 댓글 빼고 완성

--- js/travel_diary/swipe.js
+++ js/travel_diary/swipe.js
@@ -7,3 +7,13 @@ new Swiper('.diary-swiper', {
     },
     centeredSlides: true, // 1번 슬라이드가 가운데 보이기
 });
+
+new Swiper('.card-img-swiper', {
+    slidesPerView: 1, // 한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+});
