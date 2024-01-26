7292969e8f592ebb4bc1c3b5f351c5acb425584a
sg-hwang-333 <hsg151515@naver.com>
2024-01-26 09:43:32 +0900

    [FEAT] 프론트 예쁘게 수정

--- js/travel_diary/check.js
+++ js/travel_diary/check.js
@@ -12,3 +12,18 @@ function chkHeart(obj) {
         obj.src = '../../Image/icon/heart/white.png';
     }
 }
+
+function chkBookMarkFill(obj) {
+    if (obj.src.includes('un_fill')) {
+        obj.src = '../../Image/icon/bookmark/fill.png';
+    } else {
+        obj.src = '../../Image/icon/bookmark/un_fill.png';
+    }
+}
+function chkHeartFill(obj) {
+    if (obj.src.includes('un_fill')) {
+        obj.src = '../../Image/icon/heart/fill.png';
+    } else {
+        obj.src = '../../Image/icon/heart/un_fill.png';
+    }
+}
