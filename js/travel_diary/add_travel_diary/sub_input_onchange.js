28f099ba70ed7c559bb6ac590c32956f76221015
Seulgi Hwang <hsg151515@naver.com>
2024-06-09 01:26:17 +0900

    [FEAT] 글자 및 정렬 기능 추가

--- js/travel_diary/add_travel_diary/sub_input_onchange.js
+++ js/travel_diary/add_travel_diary/sub_input_onchange.js
@@ -1,9 +1,56 @@
-const text_sub_onchange = () => {
-    const subInputValue = document.querySelector('input[name="sub-input"]:checked').value;
-    console.log(subInputValue)
-    
+const containerDiv = document.getElementsByClassName('right-container')[0];
+
+const textOnChange = () => {
+    const subInputValue = document.querySelector('input[name="sub-input-text"]:checked').value;
+    let txtDiv = document.createElement('textarea');
+
     switch(subInputValue) {
         case "title": 
+        txtDiv.className = 'title-txt input-content'; 
+        txtDiv.placeholder = '제목을 입력해주세요';
+        break;
+        case "sub-title": 
+        txtDiv.className = 'sub-title-txt input-content'; 
+        txtDiv.placeholder = '부제목을 입력해주세요';
+        break;
+        case "content": 
+        txtDiv.className = 'content-txt input-content'; 
+        txtDiv.placeholder = '본문을 입력해주세요';
+        break;
+    }
+
+    containerDiv.appendChild(txtDiv);
+
+
+    txtDiv.oninput = (event) => {
+        const target = event.target;
+      
+        target.style.height = 0;
+        target.style.height = target.scrollHeight + 'px';
+    };
+
+    alignOnChange()
+}
+
+const alignOnChange = () => {
+    const subInputValue = document.querySelector('input[name="sub-input-align"]:checked').value;
+    const inputContainer = document.getElementsByClassName('input-content');
+    switch(subInputValue) {
+        case "left": 
+            for (let x of inputContainer) {
+                x.style.textAlign = "left"; 
+            }
+        break;
+        case "center":
+            for (let x of inputContainer) {
+                x.style.textAlign = "center"; 
+            }
+        break;
+        case "right": 
+            for (let x of inputContainer) {
+                x.style.textAlign = "right"; 
+            }
+        break;
     }
     
 }
