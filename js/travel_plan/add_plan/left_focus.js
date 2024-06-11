e2f9040cb6d23a8401a71cd99a16fe5511df61a3
sg-hwang-333 <hsg151515@naver.com>
2024-06-11 10:18:49 +0900

    [FEAT] + 누를 시 페이지 추가

--- js/travel_plan/add_plan/left_focus.js
+++ js/travel_plan/add_plan/left_focus.js
@@ -11,5 +11,28 @@ const titleUnFocus = () => {
 
 const leftFocus = () => {
     const leftValue = document.querySelector('input[name="left"]:checked').value;
-    console.log(leftValue);
+    switch (leftValue) {
+        case "t": break;
+    }
+}
+const addPage = () => {
+    const leftContainer = document.getElementsByClassName('left-container')[0];
+    const addPageButton = document.getElementById('add_page').parentNode;
+    const newIndex = leftContainer.children.length - 1;
+    
+    const newLabel = document.createElement('label');
+    newLabel.className = 'left-group page';
+    
+    const newInput = document.createElement('input');
+    newInput.type = 'radio';
+    newInput.name = 'left';
+    newInput.value = newIndex;
+    
+    const newSpan = document.createElement('span');
+    newSpan.textContent = newIndex;
+    
+    newLabel.appendChild(newInput);
+    newLabel.appendChild(newSpan);
+    
+    leftContainer.insertBefore(newLabel, addPageButton);
 }
