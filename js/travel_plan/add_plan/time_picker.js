6c2d08e31e9b5b172428a9527eeb869ea94cf39c
sg-hwang-333 <hsg151515@naver.com>
2024-06-11 17:13:54 +0900

    [TODO] time picker 스타일 변경하기

---
+++ js/travel_plan/add_plan/time_picker.js
@@ -0,0 +1,42 @@
+
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("openModalBtn");
+const span = document.getElementsByClassName("close")[0];
+const saveBtn = document.getElementById("saveBtn");
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+const hoursSelect = document.getElementById('hours');
+const minutesSelect = document.getElementById('minutes');
+for (let i = 1; i <= 12; i++) {
+    let option = document.createElement('option');
+    option.value = i;
+    option.text = i;
+    hoursSelect.appendChild(option);
+}
+for (let i = 0; i < 60; i++) {
+    let option = document.createElement('option');
+    option.value = i < 10 ? '0' + i : i;
+    option.text = i < 10 ? '0' + i : i;
+    minutesSelect.appendChild(option);
+}
+
+saveBtn.onclick = function () {
+    const selectedHour = hoursSelect.value;
+    const selectedMinute = minutesSelect.value;
+    const selectedAmpm = document.getElementById('ampm').value;
+    alert(`Time selected: ${selectedHour}:${selectedMinute} ${selectedAmpm}`);
+    modal.style.display = "none";
+}
