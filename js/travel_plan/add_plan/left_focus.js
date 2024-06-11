50f49dfa77701d5e1a6e349d37ed7852cde2f451
sg-hwang-333 <hsg151515@naver.com>
2024-06-11 15:12:19 +0900

    [FEAT] 날짜 radio 값마다 페이지 분리, title focus 수정

--- js/travel_plan/add_plan/left_focus.js
+++ js/travel_plan/add_plan/left_focus.js
@@ -1,38 +1,71 @@
 const inputTitle = document.getElementById('input-title');
+const writeContainer = document.getElementsByClassName('write-container')[0];
+
 
 const titleFocus = () => {
     inputTitle.classList.remove('hidden');
-    leftFocus();
+    inputTitle.focus();
+    document.getElementById('title-radio').checked = true;
+
+    for (let i = 0; i < writeContainer.children.length - 1; i++) {
+        document.getElementsByClassName('day-page')[i].classList.add('hidden');
+    }
 }
 const titleUnFocus = () => {
     inputTitle.classList.add('hidden');
-    leftFocus();
-}
-
-const leftFocus = () => {
-    const leftValue = document.querySelector('input[name="left"]:checked').value;
-    switch (leftValue) {
-        case "t": break;
+    if (document.querySelector('input[name="left"]:checked').value == 't') {
+        document.getElementById('firstPage').checked = true;
+        document.getElementsByClassName('day-page')[0].classList.remove('hidden');
     }
 }
+
+const pageFocus = (pageNum) => {
+    const pageDayDiv = document.getElementById('page-day');
+    pageDayDiv.innerHTML = pageNum;
+    titleUnFocus();
+
+    for (let i = 0; i < writeContainer.children.length - 1; i++) {
+        if (pageNum - 1 !== i) {
+            document.getElementsByClassName('day-page')[i].classList.add('hidden');
+        } else {
+            document.getElementsByClassName('day-page')[i].classList.remove('hidden');
+        }
+    }
+}
+
+
 const addPage = () => {
+    // left-container 라벨 추가
     const leftContainer = document.getElementsByClassName('left-container')[0];
     const addPageButton = document.getElementById('add_page').parentNode;
     const newIndex = leftContainer.children.length - 1;
-    
+
     const newLabel = document.createElement('label');
     newLabel.className = 'left-group page';
-    
+
     const newInput = document.createElement('input');
     newInput.type = 'radio';
     newInput.name = 'left';
     newInput.value = newIndex;
-    
+    newInput.onclick = () => {
+        pageFocus(newIndex)
+    };
+
     const newSpan = document.createElement('span');
     newSpan.textContent = newIndex;
-    
+
     newLabel.appendChild(newInput);
     newLabel.appendChild(newSpan);
-    
+
     leftContainer.insertBefore(newLabel, addPageButton);
+
+
+    // write-container 일정 추가
+    const dayPage = document.createElement('div');
+    dayPage.className = `day-page page${newIndex}`
+    dayPage.innerHTML = `${newIndex}일차 일정`;
+    newInput.checked = true;
+    writeContainer.appendChild(dayPage);
+    titleUnFocus();
+    pageFocus(newIndex);
 }
