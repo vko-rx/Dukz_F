86203d50d92ca169d61e776e7e7b0b885e6d788e
sg-hwang-333 <hsg151515@naver.com>
2024-06-12 11:36:07 +0900

    [FEAT] 일정 - 시간 선택 입력 완료 (유효성 검사 구현 X)

--- js/travel_plan/add_plan/time_picker.js
+++ js/travel_plan/add_plan/time_picker.js
@@ -1,6 +1,5 @@
-
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("openModalBtn");
+const modal = document.getElementById("time-modal");
+const btn = document.getElementById("open-time-modal");
 const span = document.getElementsByClassName("close")[0];
 const saveBtn = document.getElementById("saveBtn");
 
@@ -18,24 +17,65 @@ window.onclick = function (event) {
     }
 }
 
-const hoursSelect = document.getElementById('hours');
-const minutesSelect = document.getElementById('minutes');
+const startHoursSelect = document.getElementsByClassName('hours')[0];
+const startMinutesSelect = document.getElementsByClassName('minutes')[0];
+
+const endHoursSelect = document.getElementsByClassName('hours')[1];
+const endMinutesSelect = document.getElementsByClassName('minutes')[1];
+
 for (let i = 1; i <= 12; i++) {
-    let option = document.createElement('option');
-    option.value = i;
-    option.text = i;
-    hoursSelect.appendChild(option);
+    let startOption = document.createElement('option');
+    startOption.value = i;
+    startOption.text = i;
+
+    let endOption = document.createElement('option');
+    endOption.value = i;
+    endOption.text = i;
+
+    startHoursSelect.appendChild(startOption);
+    endHoursSelect.appendChild(endOption);
 }
-for (let i = 0; i < 60; i+=10) {
-    let option = document.createElement('option');
-    option.value = i < 10 ? '0' + i : i;
-    option.text = i < 10 ? '0' + i : i;
-    minutesSelect.appendChild(option);
+for (let i = 0; i < 60; i += 10) {
+    let startOption = document.createElement('option');
+    startOption.value = i < 10 ? '0' + i : i;
+    startOption.text = i < 10 ? '0' + i : i;
+
+    let endOption = document.createElement('option');
+    endOption.value = i < 10 ? '0' + i : i;
+    endOption.text = i < 10 ? '0' + i : i;
+
+    startMinutesSelect.appendChild(startOption);
+    endMinutesSelect.appendChild(endOption);
 }
 
-saveBtn.onclick = function () {
-    const selectedHour = hoursSelect.value;
-    const selectedMinute = minutesSelect.value;
-    const selectedAmpm = document.getElementById('ampm').value;
-    // 이 밑으로 서버 작업하면 됨
+
+saveBtn.onclick = () => {
+    let selectedHourStart = parseInt(startHoursSelect.value);
+    let selectedHourEnd = parseInt(endHoursSelect.value);
+    let selectedMinuteStart = startMinutesSelect.value;
+    let selectedMinuteEnd = endMinutesSelect.value;
+    let selectedAmpmStart = document.getElementsByClassName('ampm')[0].value;
+    let selectedAmpmEnd = document.getElementsByClassName('ampm')[1].value;
+
+    let nowPage;
+
+    for (let pageDiv of writeContainer.children) {
+        if (!pageDiv.classList.contains('hidden')) {
+            nowPage = pageDiv;
+            break;
+        }
+    }
+
+    let timeDiv = document.createElement('div');
+    timeDiv.className = 'time-text';
+    if (selectedAmpmStart == 'PM') selectedHourStart += 12;
+    if (selectedAmpmEnd == 'PM') selectedHourEnd += 12;
+    if (selectedHourStart.toString().length == 1) selectedHourStart = `0${selectedHourStart}`;
+    if (selectedHourEnd.toString().length == 1) selectedHourEnd = `0${selectedHourEnd}`;
+
+
+    timeDiv.innerHTML = `${selectedHourStart}:${selectedMinuteStart}~${selectedHourEnd}:${selectedMinuteEnd}`
+    nowPage.appendChild(timeDiv);
+
+        modal.style.display = 'none';
 }
