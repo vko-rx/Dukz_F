277e70cf6a65aef56d788e9e384667f4e5f219cf
Jian Jang <pureunnabi@gmail.com>
2024-06-17 23:52:58 +0900

    일정 페이지에 사용자 이름 불러오기

--- js/travel_plan/swipe.js
+++ js/travel_plan/swipe.js
@@ -6,3 +6,27 @@ new Swiper('.plan-swiper', {
     },
     centeredSlides: true, // 1번 슬라이드가 가운데 보이기
 });
+
+function getName() {
+    const userid = localStorage.getItem("userid");
+    const usernick = document.getElementsByClassName('user-nick');
+  
+    axios
+    .post("http://localhost:3000/user/getName", {
+        userid
+    })
+    .then((response) => {
+        const name = response.data.name;
+
+        console.log(name);
+
+        for (let i in usernick) {
+            usernick[i].innerHTML = name;
+        }
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+}
+
+getName();
