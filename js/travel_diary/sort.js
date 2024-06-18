d6b93f5582091dda73d90e07620a4d198a14c394
sg-hwang-333 <hsg151515@naver.com>
2024-06-18 13:04:21 +0900

    [FEAT] diary 더보기 페이지 일지 뜨게 하기, 이미지&제목&내용 유효성 검사

--- js/travel_diary/sort.js
+++ js/travel_diary/sort.js
@@ -7,17 +7,56 @@ function modalVisible() {
     }
 }
 
+const allDiaryDiv = document.getElementsByClassName('diary-container')[0];
+
 function getAllDiaries() {
 
     axios.post("http://54.180.238.52:3000/user/getAllDiaries", {
-        
+
     })
-    .then((response) => {
-        console.log('Successfully fetched diaries:', response.data.diaries);
-    })
-    .catch((error) => {
-        console.error('Error fetching diaries:', error);
-    });
+        .then((response) => {
+            console.log('Successfully fetched diaries:', response.data.diaries);
+            for (let x of response.data.diaries) {
+                // console.log(x)
+                let resImageSrc = '';
+                let resTitle = '';
+                let resContent = '';
+                let hashTag = '';
+                
+                for (let con of x.content) {
+                    // console.log(con.content);
+                    switch (con.contentType) {
+                        case "title": resTitle = con.content; break;
+                        case "image": resImageSrc = `http://54.180.238.52:3000${con.imageSrc}`; break;
+                        case "content": resContent = con.content; break;
+                    }
+                }
+
+                if (resTitle == '') resTitle = '제목이 없는 글이에요!';
+                if (resImageSrc == '') resImageSrc = "../../Image/dukduk/nothing.png";
+                if (resContent == '') resContent = '내용이 없는 글이에요!';
+
+                let placeDiv = document.createElement('div');
+                placeDiv.classList += 'place-container';
+                placeDiv.innerHTML = `<img class="place-img" src="${resImageSrc}">
+                                        <div class="place-txt-container">
+                                            <div class="line-container">
+                                                <div class="place-title">${resTitle}</div>
+                                                <img class="map-icon" src="../../Image/icon/map.png">
+                                            </div>
+                                            <div class="place-info">${resContent}</div>
+                                            <div class="hashtag-container place-tag">
+                                                <span>#트레저_월드맵</span>
+                                                <span>#트월맵</span>
+                                                <span>#오사카</span>
+                                            </div>
+                                        </div>`
+                allDiaryDiv.append(placeDiv)
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching diaries:', error);
+        });
 }
 
 getAllDiaries();
